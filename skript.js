e1ff4931f8aac7165dd7ff2ca06222dc57252228
youna-beep <evasypkina@gmail.com>
2025-04-10 20:03:25 +0200

    home work

--- skript.js
+++ skript.js
@@ -8,24 +8,21 @@ function handlescroll() {
 
     if (window.scrollY < screenHeight){
        newButton = buttons[0]
-    }
-    else if (window.scrollY > screenHeight && window.scrollY < screenHeight*2){
+    } else if (window.scrollY >= screenHeight && window.scrollY < screenHeight*2){
         newButton = buttons[1]
-    }
-    else if (window.scrollY >=  screenHeight*2 && window.scrollY < screenHeight*3) {
+    } else if (window.scrollY >=  screenHeight*2 && window.scrollY < screenHeight*3) {
         newButton = buttons[2];
     } else if (window.scrollY >= screenHeight*3 && window.scrollY < screenHeight*4) {
         newButton = buttons[3];
     } else {
         newButton = buttons[4]; 
     }
+    if ( newButton) {
+        oldButton.classList.remove("main_activ_button");
+        newButton.classList.add("main_activ_button");
+    }
 }
 
 window.onscroll = handlescroll
 
-if ( newButton !== oldButton) {
-    if (oldButton) {
-        oldButton.classList.remove("main_activ_button");
-    }
-    newButton.classList.add("main_activ_button");
-}
+
