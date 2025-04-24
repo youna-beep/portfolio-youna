ac180d5e62ef0bfadb9868aede52092eeb140b8b
youna-beep <evasypkina@gmail.com>
2025-04-24 20:04:50 +0200

    update comments

--- skript.js
+++ skript.js
@@ -1,6 +1,7 @@
 const buttons = document.querySelectorAll(".menu__links")
 const menu_button = document.querySelector(".mobile_menu")
-const menu_open = document.querySelector(".open_menu")
+const mobile_menu = document.querySelector(".open_menu")
+const icon = document.quaerySelector(".icon")
 
 function handlescroll() {
     const screenHeight = document.documentElement.clientHeight;
@@ -26,4 +27,12 @@ function handlescroll() {
 
 window.onscroll = handlescroll
 
-
+function toggleMenu() {
+    if (mobile_menu.classList.contains("open_menu_active")){
+        icon.src = 'img/mobile_menu.png'
+    } else {
+        icon.src = "img/close_menu.png"
+    }
+    mobile_menu.classList.toggle("open_menu_active")
+}
+menu_button.onclick = toggleMenu
