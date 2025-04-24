c8566c8e69b1c46fabcb9aef2763fa771b99cf16
youna-beep <evasypkina@gmail.com>
2025-04-24 20:32:52 +0200

    FIRST RELIASE

--- skript.js
+++ skript.js
@@ -1,28 +1,35 @@
 const buttons = document.querySelectorAll(".menu__links")
 const menu_button = document.querySelector(".mobile_menu")
+const buttons_mobile = document.querySelectorAll(".open_menu > .menu__links")
 const mobile_menu = document.querySelector(".open_menu")
-const icon = document.quaerySelector(".icon")
+const icon = document.querySelector(".icon")
 
 function handlescroll() {
     const screenHeight = document.documentElement.clientHeight;
-    let newButton = null;
+    
     const oldButton = document.querySelector(".main_activ_button")
+    const oldButton_mobile = document.querySelector (".open_menu > .main_activ_button")
+    let index = 0
 
     if (window.scrollY < screenHeight){
-       newButton = buttons[0]
+        index = 0
     } else if (window.scrollY >= screenHeight && window.scrollY < screenHeight*2){
-        newButton = buttons[1]
+        index = 1
     } else if (window.scrollY >=  screenHeight*2 && window.scrollY < screenHeight*3) {
-        newButton = buttons[2];
+        index = 2
     } else if (window.scrollY >= screenHeight*3 && window.scrollY < screenHeight*4) {
-        newButton = buttons[3];
+        index = 3
     } else {
-        newButton = buttons[4]; 
+        index = 4
     }
-    if ( newButton) {
-        oldButton.classList.remove("main_activ_button");
-        newButton.classList.add("main_activ_button");
-    }
+    
+    let newButton = buttons[index]
+    let newButton_mobile = buttons_mobile[index]
+
+    oldButton.classList.remove("main_activ_button");
+    newButton.classList.add("main_activ_button"); 
+    oldButton_mobile.classList.remove("main_activ_button");
+    newButton_mobile.classList.add("main_activ_button"); 
 }
 
 window.onscroll = handlescroll
